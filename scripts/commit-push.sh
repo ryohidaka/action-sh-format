b88604116a6af4b842f4b5c597a152c8e964000d
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-09-12 12:44:50 +0900

    refact: Fix comment.

--- scripts/commit-push.sh
+++ scripts/commit-push.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -e # Exit immediately if any command exits with a non-zero status
 
-# Get the directory path from the first argument
+# Get arguments from the inputs
 path="${1}"
-
-# Get the name from the second argument
 name="${2}"
-
-# Get the email from the third argument
 email="${3}"
-
-# Get the commit message from the fourth argument
 message="${4}"
 
 # Configure git user name locally for this repository
